b6ab60b861ea968f0f635a5f58015470d1214fc4
neilsatt <neilsatt@gmail.com>
2018-02-18 19:52:53 -0700

    update webpack config

--- wordcounter/webpack.config.js
+++ wordcounter/webpack.config.js
@@ -2,10 +2,21 @@ const path = require('path');
 
 module.exports = {
     entry: { app: './src/index.js' },
-};
-
-output: {
+    output: {
     path: path.resolve(__dirname),
     filename: '[name]-bundle.js'
-}
+    },
+    
+    module: {
+        rules: [
+            {
+                test:/\.js$/,
+                include: path.resolve(__dirname, 'src'),
+                use: [babel-loader]
+            }        
+        ]  
+    }   
+};
+
+
 
